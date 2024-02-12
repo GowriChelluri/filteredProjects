a6b56770b6d01aa3cbf59f145044141183f05dac
GowriChelluri <gowrichelluri08@gmail.com>
2024-02-12 14:55:43 +0000

    Commited

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, clickTabItem} = props
+  const {tabId, displayText} = tabDetails
+  const onClickTabItem = () => {
+    clickTabItem(tabId)
+  }
+  return (
+    <li className="tab-item-container ">
+      <button type="button" className="tab-btn" onClick={onClickTabItem}>
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
